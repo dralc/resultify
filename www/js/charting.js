048f2d511d9e50717f77d7a5d999acebc6ec419a
Chan, Aloysius <aloysius.chan@news.com.au>
2017-03-10 13:00:54 +1100

    - added bar chart

--- www/js/charting.js
+++ www/js/charting.js
@@ -1,9 +1,12 @@
 
+Chart.defaults.global.defaultFontColor = '#fff';
+
 function initCharts(data) {
     createPie('myChart1', ['Red', 'Blue'], data.a);
     createPie('myChart2', ['Red', 'Blue'], data.b);
     createPie('myChart3', ['Red', 'Blue'], data.c);
     createPie('myChart4', ['Red', 'Blue'], data.d);
+    createBar('myChartBar1', 'score', ['John', 'Dick', 'Harry'], data.e);
 }
 
 function createPie(context, labelAr, dataAr) {
@@ -25,4 +28,38 @@ function createPie(context, labelAr, dataAr) {
         },
         options: { responsive: false}
     });
+}
+
+function createBar(context, legendLabel, yLabelAr, dataAr) {
+    const chart = new Chart(context, {
+        type: 'horizontalBar',
+        data: {
+            labels: yLabelAr,
+            datasets: [
+                {
+                    label: legendLabel,
+                    backgroundColor: [
+                        '#FEF660',
+                        '#C6EF6A',
+                        '#92E67C',
+                        '#5CDA92',
+                        '#21CEA4'
+                    ],
+                    borderWidth: 1,
+                    data: dataAr
+                }
+            ]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
 }
